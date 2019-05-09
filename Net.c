4ec895941db4118a26748142f9bec7f4e490c059
BryceKopan <BryceKopan@gmail.com>
2019-05-08 20:27:35 -0500

    Updated net.c

--- Net.c
+++ Net.c
@@ -7,55 +7,67 @@
 
 int createSocket(int port)
 {
+    //Create socket for writing
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     {
         return -1;
     }
-
+    
+    //Create multicast address for writing
     struct sockaddr_in groupSock;
     memset(&groupSock, 0, sizeof(groupSock));
     groupSock.sin_family = AF_INET;
     groupSock.sin_addr.s_addr = inet_addr(MULTICAST_GROUP);
     groupSock.sin_port = htons(port);
-
+    
+    //Set writing to loopback interface
     struct in_addr localInterface;
     localInterface.s_addr = inet_addr("127.0.0.1");
     if(setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0)
     {
         return -1;
     }
-
-    if (connect(sockfd, (const struct sockaddr *)&groupSock, sizeof(groupSock)))
+    
+    //Connect the socket for writing on the multicast loopback interface
+    if (connect(sockfd, (struct sockaddr *)&groupSock, sizeof(groupSock)))
     {
         return -1;
     }
-
+    
     return sockfd;
 }
 
 int createReadSocket(int port)
 {
+    //Create socket for reading
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     {
         return -1;
     }
-
+    
+    //Set reuse port so others can read on the selected port
     int one = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one));
-
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one)) < 0)
+        return -1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &one, sizeof(one)) < 0)
+        return -1;
+    
+    //Create address to read on
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(port);
-
-    if (bind(sockfd, (const struct sockaddr *)&addr, sizeof(addr)) < 0)
+    
+    //Bind the socket to the address
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         return -1;
     }
-
+    
+    //Allow the socket to read on muilticast
     struct ip_mreq group;
     group.imr_multiaddr.s_addr = inet_addr(MULTICAST_GROUP);
     group.imr_interface.s_addr = inet_addr("127.0.0.1");
@@ -63,6 +75,6 @@ int createReadSocket(int port)
     {
         return -1;
     }
-
+    
     return sockfd;
 }
